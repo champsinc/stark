0b4cf634c95aff866068f5fcd7007af6ecabdd95
nirbhay <pherwani37@gmail.com>
2019-05-27 21:00:01 -0400

    Updates

---
+++ precache-manifest.adbd805e1c9b3e6f3508b3fd0c311019.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = [
+  {
+    "revision": "defbb5912484b376c75f8235e1830b4c",
+    "url": "/stark/static/media/NoirPro-Light.defbb591.eot"
+  },
+  {
+    "revision": "02b3ed7a57b747ca490c",
+    "url": "/stark/static/css/main.5b249424.chunk.css"
+  },
+  {
+    "revision": "686337dbc2e904073df0",
+    "url": "/stark/static/js/runtime~main.858edf26.js"
+  },
+  {
+    "revision": "a302e546bfe8cef2a36504d844542fa4",
+    "url": "/stark/static/media/NoirPro-Heavy.a302e546.woff"
+  },
+  {
+    "revision": "15a63006cab4bf6e0073",
+    "url": "/stark/static/js/2.62c5978d.chunk.js"
+  },
+  {
+    "revision": "192e3354b379ce6e3b36",
+    "url": "/stark/static/js/3.1a90019c.chunk.js"
+  },
+  {
+    "revision": "b8dff027a673eafe38a7",
+    "url": "/stark/static/js/4.0d77fe57.chunk.js"
+  },
+  {
+    "revision": "74caa41d794fb97ca5480b4ad0a2db03",
+    "url": "/stark/static/media/loader.74caa41d.svg"
+  },
+  {
+    "revision": "7ed12ca430db89e02f473502648eadc5",
+    "url": "/stark/static/media/sprite-flags-24x24.7ed12ca4.png"
+  },
+  {
+    "revision": "36251c7bf6634a7cd87cf02935463f61",
+    "url": "/stark/static/media/gaxon.36251c7b.woff"
+  },
+  {
+    "revision": "00fcca8e4896ea70aeaf79cc01482b54",
+    "url": "/stark/static/media/gaxon.00fcca8e.ttf"
+  },
+  {
+    "revision": "32cebc42775933015139f56793bcdca8",
+    "url": "/stark/static/media/gaxon.32cebc42.eot"
+  },
+  {
+    "revision": "b1a63de8de332478102c86d8481bafc2",
+    "url": "/stark/static/media/gaxon.b1a63de8.svg"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/stark/static/media/notification.c0d3c94c.eot"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/stark/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/stark/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/stark/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "752c13e1b881564f5f54afa1a4859492",
+    "url": "/stark/static/media/NoirPro-Regular.752c13e1.eot"
+  },
+  {
+    "revision": "02b3ed7a57b747ca490c",
+    "url": "/stark/static/js/main.b02ef935.chunk.js"
+  },
+  {
+    "revision": "251883125165605d8c409595ff236734",
+    "url": "/stark/static/media/NoirPro-Light.25188312.woff2"
+  },
+  {
+    "revision": "fe70663122656a5c59de60232d8be61f",
+    "url": "/stark/static/media/NoirPro-Regular.fe706631.woff2"
+  },
+  {
+    "revision": "3d82e791b707535b54dd0d68ba38cdbc",
+    "url": "/stark/static/media/NoirPro-Medium.3d82e791.woff2"
+  },
+  {
+    "revision": "e928d2a22faf75b6884fd3b4763bdcee",
+    "url": "/stark/static/media/NoirPro-SemiBold.e928d2a2.woff2"
+  },
+  {
+    "revision": "bb9d0cc624e14a907449e5ea1f602186",
+    "url": "/stark/static/media/NoirPro-Bold.bb9d0cc6.woff2"
+  },
+  {
+    "revision": "873c8fa8f69d6a2118f1119e1e80eb74",
+    "url": "/stark/static/media/NoirPro-Heavy.873c8fa8.woff2"
+  },
+  {
+    "revision": "bbd56c69aef4c37772e2305a2c2cd422",
+    "url": "/stark/static/media/NoirPro-Light.bbd56c69.woff"
+  },
+  {
+    "revision": "83b986718958eae204ae595909a4d66e",
+    "url": "/stark/static/media/NoirPro-Regular.83b98671.woff"
+  },
+  {
+    "revision": "6d74487df6dc4e67a2f4c3fd8650362c",
+    "url": "/stark/static/media/NoirPro-Medium.6d74487d.eot"
+  },
+  {
+    "revision": "d956581657d758fdda26a51e591a8a85",
+    "url": "/stark/static/media/NoirPro-Bold.d9565816.eot"
+  },
+  {
+    "revision": "ac97525f7e84a8b62907058d6aa4e93c",
+    "url": "/stark/static/media/NoirPro-SemiBold.ac97525f.eot"
+  },
+  {
+    "revision": "7194b3b5891959a3e98285e89bdc01c6",
+    "url": "/stark/static/media/NoirPro-Heavy.7194b3b5.eot"
+  },
+  {
+    "revision": "04b5d842dfad0fbb345cda0473c72d0e",
+    "url": "/stark/static/media/NoirPro-Medium.04b5d842.woff"
+  },
+  {
+    "revision": "1bc66a4b5cd48c729a854842fd066a1b",
+    "url": "/stark/static/media/NoirPro-SemiBold.1bc66a4b.woff"
+  },
+  {
+    "revision": "9dbcc95df5c86335f0e083c353c51239",
+    "url": "/stark/static/media/NoirPro-Bold.9dbcc95d.woff"
+  },
+  {
+    "revision": "15a63006cab4bf6e0073",
+    "url": "/stark/static/css/2.dd382abf.chunk.css"
+  },
+  {
+    "revision": "19c0d917ae6cd61c930d42a95f7c9c0a",
+    "url": "/stark/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/stark/precache-manifest.b04a57ebd92722a9acd90f3a89ebf589.js"
+  "/stark/precache-manifest.adbd805e1c9b3e6f3508b3fd0c311019.js"
 );
 
 workbox.clientsClaim();
